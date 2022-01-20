5db26e1710bff2a70e4f93f20c69e04eeb806050
maxi giuliano <maxigiuliano18@gmail.com>
2022-01-20 00:31:20 -0300

    funcionalidades para js

--- modulo 2/unidad 4/js/main.js
+++ modulo 2/unidad 4/js/main.js
@@ -1,6 +1,6 @@
-
-
 const toggleButton = document.getElementById('button-menu')
+const toggleButtonContact = document.getElementById('button-formulario')
+const toggleButtonHome = document.getElementById('button-inicio')
 const navWrapper = document.getElementById('nav')
 
 toggleButton.addEventListener('click',() => {
@@ -8,6 +8,16 @@ toggleButton.addEventListener('click',() => {
   navWrapper.classList.toggle('show')
   $("#nav").removeClass("justify-content-end")
   $("#nav").addClass("flex-column justify-content-start")
+})
+
+toggleButtonContact.addEventListener('click',() => {
+  toggleButton.classList.toggle('close')
+  navWrapper.classList.toggle('show')
+})
+
+toggleButtonHome.addEventListener('click',() => {
+  toggleButton.classList.toggle('close')
+  navWrapper.classList.toggle('show')
 })
 
 navWrapper.addEventListener('click',e => {
