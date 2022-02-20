dff60c37df0557e25af26594cf5d22116254836f
maxi giuliano <maxigiuliano18@gmail.com>
2022-02-20 00:23:28 -0300

    ejercicio 3 unidad 4 modulo 3

--- modulo 3/unidad 4/js/main.js
+++ modulo 3/unidad 4/js/main.js
@@ -3,3 +3,71 @@
         var body = document.getElementById("body")
             body.style.backgroundColor = color
       };
+
+
+      var alumnos = [
+
+        {
+      
+          nombre: 'Juan Gomez',
+      
+          nota: 7
+      
+        }, {
+      
+          nombre: 'Pedro Rodriguez',
+      
+          nota: 4
+      
+        }, {
+      
+          nombre: 'Roxana García',
+      
+          nota: 8
+      
+        }, {
+      
+          nombre: 'Luciano Lopez',
+      
+          nota: 5
+      
+        }, {
+      
+          nombre: 'Fernanda Gimenez',
+      
+          nota: 6
+      
+        }, {
+      
+          nombre: 'Florencia Martinez',
+      
+          nota: 10
+      
+        }, {
+      
+          nombre: 'Raul Sanchez',
+      
+          nota: 7
+      
+        }, {
+      
+          nombre: 'Sandra Figueroa',
+      
+          nota: 8
+      
+        }
+      
+      ];
+
+      function principal(){
+        var texto = document.getElementById('texto')
+        var contador = document.getElementById('contador')
+        texto.addEventListener('keyup', function(){
+          contador.innerText = texto.value.length
+        })
+        
+        var aprobados = alumnos.filter( alumno => alumno.nota >= 7)
+        for (const alumno of aprobados) {
+          console.log(alumno.nombre +' Aprobado con: '+alumno.nota)
+        }
+      }
